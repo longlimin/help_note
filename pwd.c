03f5ab478351072fec7304f42388bfec4a534ef4
chenpenghui <chenpenghui@ruaho.com>
2018-04-08 10:59:17 +0800

    新增pwd记录文件

---
+++ pwd.c
@@ -0,0 +1,30 @@
+/*
+记录需要用的 地址 账号 密码
+
+
+*/
+1234qwer
+pai3-6
+uppstr
+
+//微软
+chen_peng_hui@outlook.com
+
+
+
+
+
+
+//翻墙
+插件 switchyOmega.crx 网盘
+软件 shadowsock.exe   网盘/github
+    ubuntu_server
+    140.82.3.69
+    8989
+    yw020154
+    127.0.0.1
+    1080
+    socks
+    aes-256-cfb
+
+代理配置 Omega.*.bak 网盘
