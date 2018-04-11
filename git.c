f891d3e76abed3cacfe7cba90eaeafec6d968971
Walker <1424234500@qq.com>
2018-04-11 09:18:01 +0800

    Wed Apr 11 09:18:01 DST 2018

--- git.c
+++ git.c
@@ -193,3 +193,13 @@ b.--> git push origin master
 
  
 
+方法一 通过命令直接修改远程地址
+进入git_test根目录
+git remote 查看所有远程仓库， git remote xxx 查看指定远程仓库地址
+git remote set-url origin http://192.168.100.235:9797/john/git_test.git
+方法二 通过命令先删除再添加远程仓库
+
+进入git_test根目录
+git remote 查看所有远程仓库， git remote xxx 查看指定远程仓库地址
+git remote rm origin
+git remote add origin http://192.168.100.235:9797/john/git_test.git
