e62e9b5aa48097831fc0972999b7e0a4e0f34b87
Walker <1424234500@qq.com>
2018-05-29 19:42:31 +0800

    Tue May 29 19:42:31 DST 2018

--- cmd/ss.bat
+++ cmd/ss.bat
@@ -16,12 +16,12 @@ tasklist|find /i "QQ.exe" || start "QQ" "C:\Program Files (x86)\Tencent\QQ\Bin\Q
 tasklist|find /i "Foxmail" || start "foxmail" "D:\Program Files\Foxmail 7.2\Foxmail"
  
 ::eclipse
-tasklist|find /i "eclipse.exe" || start "eclipse proj" "C:\Users\ThinkPad\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\eclipse" 
+::tasklist|find /i "eclipse.exe" || start "eclipse proj" "C:\Users\ThinkPad\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\eclipse" 
 
 ::tasklist|find /i "eclipse.exe" || start "eclipse myself" "D:\eclipse_bak\eclipse\eclipse.exe" 
 
 ::editor
-tasklist|find /i "WebStorm.exe" || start "WebStorm" "D:\Program Files (x86)\JetBrains\WebStorm 11.0.3\bin\WebStorm.exe" 
+::tasklist|find /i "WebStorm.exe" || start "WebStorm" "D:\Program Files (x86)\JetBrains\WebStorm 11.0.3\bin\WebStorm.exe" 
 
 ::tasklist|find /i "plsqldev.exe" || start "plsqldev" "C:\Users\ThinkPad\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\plsqldev"
  
