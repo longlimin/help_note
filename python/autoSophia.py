bb090b742e6ad2b6647f6dcdbb527ed83a02ce79
Walker <1424234500@qq.com>
2018-06-25 19:05:35 +0800

    Mon Jun 25 19:05:35 DST 2018

--- python/autoSophia.py
+++ python/autoSophia.py
@@ -1,86 +1,76 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-  
-import urllib2 
+import urllib
+import urllib2
+import cookielib
+
+def httpGet(name, url, data):
+    print(name)
+    # 模拟登录，并把cookie保存到变量
+    try:
+        if(data):
+            result = opener.open(url, urllib.urlencode(data))
+        else:
+            result = opener.open(url)
+        show(result)
+    except Exception as e:
+        print(e)
+    show(result)
+
+
+# print("登录认证")
+# # 模拟登录，并把cookie保存到变量
+# result = opener.open("http://drrr.com/",
+#     urllib.urlencode({
+#             "name":"Walker004",
+#             "login":"ENTER",
+#             "token":"0e2ebc572e16d8f983f5a399b54e8823",
+#             "direct-join":"",
+#             "language":"zh-CN",
+#             "icon":"eight",
+#     })
+# )
+# show(result)
+
+
+def show(result):
+    try:
+        print(result.read())
+    except Exception as e:
+        print(e)
+    print("\n------------------------------------\n")
+
+filename = 'cookie.txt'
+# 声明一个MozillaCookieJar对象实例来保存cookie，之后写入文件
+cookie = cookielib.MozillaCookieJar(filename)
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+
+print("访问主页")
+result = opener.open("http://drrr.com/")
+cookie.save(ignore_discard=True, ignore_expires=True)
+
+show(result)
+httpGet("登录认证", "http://drrr.com/", {
+            "name":"Walker004",
+            "login":"ENTER",
+            "token":"0e2ebc572e16d8f983f5a399b54e8823",
+            "direct-join":"",
+            "language":"zh-CN",
+            "icon":"eight",
+    })
+ 
+# 保存cookie到cookie.txt中
+cookie.save(ignore_discard=True, ignore_expires=True)
+
+httpGet("获取房间列表", "http://drrr.com/lounge?api=json")
+
+httpGet("加入房间", "http://drrr.com/room/?id=cgyRhaqTvB")
+ 
 
 
 
-"""
-General
-    Request URL:http://drrr.com/
-    Request Method:GET
-    Status Code:200 OK
-    Remote Address:23.251.96.131:80
-    Referrer Policy:no-referrer-when-downgrade
-Response Headers view source
-    Access-Control-Allow-Credentials:true
-    Access-Control-Allow-Origin://drrr.com
-    Cache-Control:no-cache
-    Connection:keep-alive
-    Content-Encoding:gzip
-    Content-Type:text/html; charset=UTF-8
-    Date:Fri, 22 Jun 2018 10:24:55 GMT
-    Expires:Fri, 22 Jun 2018 10:24:54 GMT
-    Pragma:no-cache
-    Server:nginx
-    Strict-Transport-Security:max-age=86400; includeSubDomains; preload
-    Transfer-Encoding:chunked
-    Vary:Accept-Encoding
-    X-Cache:MISS from 001.mul.lax01.us.krill.zenlogic.net
-    X-ORCA-Accelerator:MISS from 001.mul.lax01.us.krill.zenlogic.net
-Request Headers view source
-    Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-    Accept-Encoding:gzip, deflate
-    Accept-Language:zh-CN,zh;q=0.9
-    Cache-Control:no-cache
-    Connection:keep-alive
-    Cookie:_ga=GA1.2.1468643479.1529641802; _gid=GA1.2.1221378764.1529641802; drrr-session-1=lsi8ru9ciedcibkl7kcg78ujo5; _gat=1
-    DNT:1
-    Host:drrr.com
-    Pragma:no-cache
-    Referer:http://drrr.com/lounge
-    Upgrade-Insecure-Requests:1
-    User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36
-"""
 
-
-  
-# 设置浏览器请求头  
-headers={  
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate",
-    "Accept-Language":"zh-CN,zh;q=0.9",
-    "Cache-Control":"no-cache",
-    "Connection":"keep-alive",
-    "Cookie":"_ga=GA1.2.1468643479.1529641802; _gid=GA1.2.1221378764.1529641802; drrr-session-1=lsi8ru9ciedcibkl7kcg78ujo5; _gat=1",
-    "DNT":"1",
-    "Host":"drrr.com",
-    "Pragma":"no-cache",
-    "Referer":"http://drrr.com/lounge",
-    "Upgrade-Insecure-Requests":"1",
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-}  
-  
-#建立请求内容  
-request=urllib2.Request("http://drrr.com",headers=headers)  
-  
-#获取响应  
-response=urllib2.urlopen(request)  
-  
-#页面内容  
-html=response.read()  
-  
-# print html  
-
-print response.getcode() #返回响应码  
-print response.geturl() #返回实际url  
-print response.info() #返回服务器响应的报头  
-print "---------------"
-print(response)
-fo = open("/mnt/e/test.html", "w")
-fo.write(html.toString())
-
-if __name__ == '__main__':
+print("等待------")
+while(True):
     pass
-    # AutoSophia()
 
-        
