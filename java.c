0153c14373ef596fd0a21f3f3a5cf325f853dcfc
walker <1424234500@qq.com>
2018-10-12 09:50:06 +0800

    Fri Oct 12 09:50:06 DST 2018

---
+++ java.c
@@ -0,0 +1,30 @@
+j
+
+
+
+
+
+
+JAVA_HOME
+C:\Program Files\Java\jdk-9
+PATH
+%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin
+
+
+javap -v ServiceImpl.class
+javap -verbose ServiceImpl.class
+
+
+
+J2SE 8 = 52,
+J2SE 7 = 51,
+J2SE 6.0 = 50,
+J2SE 5.0 = 49,
+JDK 1.4 = 48,
+JDK 1.3 = 47,
+JDK 1.2 = 46,
+JDK 1.1 = 45
+
+
+
+
