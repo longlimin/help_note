2e521f360b33c0fa544f923f61b5609adb0f2ee7
walker <1424234500@qq.com>
2018-12-10 17:23:40 +0800

    Mon Dec 10 17:23:40 DST 2018

--- cmd/ss.bat
+++ cmd/ss.bat
@@ -12,27 +12,27 @@ tasklist|find /i "chrome.exe" || start "google chrome" "C:\Program Files (x86)\G
 tasklist|find /i "360chrome.exe" || start "360chrome" "D:\360\360Chrome\Chrome\Application\360chrome" 
 
 ::tasklist|find /i "TIM.exe" || start "TIM" "C:\Users\ThinkPad\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\TIM"
-tasklist|find /i "QQ.exe" || start "QQ" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ"
+rem tasklist|find /i "QQ.exe" || start "QQ" "C:\Program Files (x86)\Tencent\QQ\Bin\QQ"
 
 tasklist|find /i "Foxmail" || start "foxmail" "D:\Program Files\Foxmail 7.2\Foxmail"
  
 ::eclipse
-tasklist|find /i "eclipse.exe" || start "eclipse proj" "C:\Users\ThinkPad\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\eclipse" 
+tasklist|find /i "eclipse.exe" || start "eclipse proj" "D:\eclipse_bak\eclipse\eclipse.exe" 
 
 ::tasklist|find /i "eclipse.exe" || start "eclipse myself" "D:\eclipse_bak\eclipse\eclipse.exe" 
 
 ::editor
-tasklist|find /i "WebStorm.exe" || start "WebStorm" "D:\Program Files (x86)\JetBrains\WebStorm 11.0.3\bin\WebStorm.exe" 
+rem tasklist|find /i "WebStorm.exe" || start "WebStorm" "D:\Program Files (x86)\JetBrains\WebStorm 11.0.3\bin\WebStorm.exe" 
 
 tasklist|find /i "plsqldev.exe" || start "plsqldev" "C:\Users\ThinkPad\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\plsqldev"
  
 
 ::github tool
-tasklist|find /i "SourceTree" || start "SourceTree" "C:\Users\ThinkPad\AppData\Local\SourceTree\SourceTree.exe" 
+rem tasklist|find /i "SourceTree" || start "SourceTree" "C:\Users\ThinkPad\AppData\Local\SourceTree\SourceTree.exe" 
 
 
 ::echat_desktop proj
-call no
+rem call no
 call ng
 
 
