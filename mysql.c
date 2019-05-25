f371324889e2c8472fe24283fd8c9217e918346f
walker <1424234500@qq.com>
2019-05-25 15:53:16 +0800

    2019年 05月 25日 星期六 15:53:15 CST

--- mysql.c
+++ mysql.c
@@ -47,11 +47,23 @@ show create table student;  //查看表create创建语句
 
 insert into test values('001', 'walker');
 update test set name='walker1';
+//分页查询
 select * from test;
 select * from test limit 0,1;
-
-select COLUMN_NAME from information_schema.COLUMNS where table_name = 'test';   //查看列名
+//查看列名
+select COLUMN_NAME from information_schema.COLUMNS where table_name = 'test';   
 NOT NULL auto_increment,
+
+//常用函数 
+ifnull  nvl
+
+//查询
+//行号rownum
+select rownum from (select  (@i:=@i+1) rownum from  information_schema.COLUMNS t ,(select   @i:=0) it ) t  where rownum < 10 ;       
+select lpad(level, 2, '0') lev from (select  (@i:=@i+1) level from  information_schema.COLUMNS t ,(select   @i:=0) it ) t  where level<=24     ;
+//代码java执行替换
+select lpad(level, 2, '0') lev from (select  (@i/*'*/:=/*'*/@i+1) level from  information_schema.COLUMNS t ,(select   @i/*'*/:=/*'*/0) it ) t  where level<=24     ;
+用符号:/*'*/:=/*'*/转换:=
 
 //安全模式
 safemode
@@ -89,6 +101,29 @@ system > const > eq_ref > ref > fulltext > ref_or_null > index_merge > unique_su
 //表锁：开销小 加锁快 不会出现死锁
 //行锁：开销大 加锁慢 会出现死锁 锁定力度小 发生锁冲突概率小
 show engines;
+
+desc 表名;       // 表信息 
+show columns from 表名;       // 表字段 
+describe 表名;       // 表信息 
+show create table 表名;        // 表创建语句 
+show create database 数据库名;        // 显示数据库 信息 
+show table status from 数据库名;        // 数据库状态 
+show tables或show tables from database_name;       // 显示当前数据库中所有表的名称 
+show databases;       // 显示mysql中所有数据库的名称 
+show processlist;       // 显示系统中正在运行的所有进程，也就是当前正在执行的查询。大多数用户可以查看他们自己的进程，但是如果他们拥有process权限，就可以查看所有人的进程，包括密码。 
+show table status;       // 显示当前使用或者指定的database中的每个表的信息。信息包括表类型和表的最新更新时间 
+show columns from table_name from database_name;        // 显示表中列名称 
+show columns from database_name.table_name;        // 显示表中列名称 
+show grants for user_name@localhost;        // 显示一个用户的权限，显示结果类似于grant 命令 
+show index from table_name;        // 显示表的索引 show status;解释：显示一些系统特定资源的信息，例如，正在运行的线程数量 
+show variables;        // 显示系统变量的名称和值 show privileges;解释：显示服务器所支持的不同权限 
+show create database database_name ;       // 显示create database 语句是否能够创建指定的数据库 
+show create table table_name;       // 显示create database 语句是否能够创建指定的数据库 
+show engies;        // 显示安装以后可用的存储引擎和默认引擎。 
+show innodb status ;        // 显示innoDB存储引擎的状态 
+show logs;        // 显示BDB存储引擎的日志 
+show warnings;       //显示最后一个执行的语句所产生的错误、警告和通知 
+show errors;       // 只显示最后一个执行语句所产生的错误
 
 set names utf8;
 set character set utf8;
